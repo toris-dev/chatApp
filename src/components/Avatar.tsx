1367fee36e8b5be8e4ee3764673188be42b9b12e
toris-dev <ironjustlikethat@gmail.com>
2024-04-21 16:06:21 +0900

    ✨ Feat: 대화창 유저 이미지  컴포넌트 추가

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,29 @@
-const Avatar = () => {
-  return <div>Avatar</div>;
+"use client";
+
+import { User } from "@prisma/client";
+import Image from "next/image";
+
+type AvatarProps = {
+  user: User;
+};
+
+const Avatar = ({ user }: AvatarProps) => {
+  return (
+    <div className="relative">
+      <div
+        className={`relative inline-block size-9 overflow-hidden rounded-full md:size-11`}
+      >
+        <Image
+          fill
+          src={user?.image || "/images/placeholder.png"}
+          alt="Avatar"
+        />
+      </div>
+      <span
+        className={`absolute right-0 top-0 block size-2 rounded-full bg-green-500 ring-2 ring-white md:size-3`}
+      />
+    </div>
+  );
 };
 
 export default Avatar;
--- src/components/AvatarGroup.tsx
+++ src/components/AvatarGroup.tsx
@@ -1,5 +1,35 @@
-const AvatarGroup = () => {
-  return <div>AvatarGroup</div>;
+import { User } from "@prisma/client";
+import Image from "next/image";
+import { FC } from "react";
+
+type AvatarGroupType = {
+  users?: User[];
+};
+const AvatarGroup: FC<AvatarGroupType> = ({ users = [] }) => {
+  const slicedUsers = users.slice(0, 3);
+  const positionMap = {
+    0: "top-0 left-[12px]",
+    1: "bottom-0",
+    2: "bottom-0 right-0",
+  };
+  return (
+    <div className="relative size-11">
+      {slicedUsers.map((user, index) => (
+        <div
+          key={user.id}
+          className={`absolute inline-block size-[21px] overflow-hidden rounded-full ${
+            positionMap[index as keyof typeof positionMap]
+          } `}
+        >
+          <Image
+            fill
+            src={user?.image || "/image/placeholder.png"}
+            alt="avatar"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AvatarGroup;
